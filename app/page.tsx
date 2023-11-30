f3b29d8fa614c379ac67dd741f15d59fd93a96f1
Jeffrey Ting <jting@bu.edu>
2023-11-30 15:45:15 -0500

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,16 @@ export default function Home() {
 	return (
 		<>
 			<Header />
-			<main className="w-full bg-[#00FF00]">
-				<div className="flex flex-col w-full align-center justify-between bg-[#FFFFFF]">
-					<div className="mx-10 text-center absolute left-20 top-32 w-20%">
-						<p className="my-10 text-5xl">Find your rank</p>
-						{loggedIn ? (
-							<button className="m-10 px-5 py-2 text-4xl rounded-2xl bg-[#78CB5F] text-white shadow-xl">
-								Login with Spotify
-							</button>
-						) : (
-							<ScoreBar score={score} />
-						)}
-					</div>
+			<main className="w-full">
+				<div className="flex flex-col w-full items-center justify-between">
+					<h2 className="m-5 text-6xl">Find your rank</h2>
+					{loggedIn ? (
+						<button className="m-5 px-5 py-2 w-fit text-2xl rounded-2xl bg-[#78CB5F]">
+							Login with Spotify
+						</button>
+					) : (
+						<ScoreBar score={score} />
+					)}
 				</div>
 			</main>
 		</>
--- components/score-bar.tsx
+++ components/score-bar.tsx
@@ -1,6 +1,6 @@
 export const ScoreBar = ({ score }: { score: number }) => {
 	return (
-		<div className="flex flex-row justify-start items center w-[70%] bg-[#00FF00]">
+		<div className="flex flex-row justify-start items center w-[40%] bg-[#00FF00]">
 			<div className="bg-[#FF0000] h-[90%" style={{ width: `${score}%` }}>
 				<p>{score}%</p>
 			</div>

38dd2fa89564c89668b5aa59928249348152cc71
KingTingTheGreat <jting@bu.edu>
2023-12-17 03:32:36 -0500

    connects to user data stored in database

--- app/rankings/page.tsx
+++ app/rankings/page.tsx
@@ -1,5 +1,33 @@
 import { Leaderboard } from "@/components/leaderboard";
 
-export default function Rankings() {
-	return <Leaderboard />;
+// const axios = require("axios");
+
+const processUsers = (users: any) => {
+	return users.map((user: any) => {
+		return {
+			username: user.username,
+			score: Number(user.score),
+		};
+	});
+};
+
+export default async function Rankings() {
+	try {
+		// const recipesResponse = await axios.get("http://localhost:3000/api/users"); // replace with deployment endpoint
+		// const recipes = recipesResponse.data;
+		// const users = processUsers(recipes);
+		// if (!(recipes instanceof Array)) {
+		// 	throw new Error("Recipes response data is not an array");
+		// }
+
+		const users = await fetch("http://localhost:3000/api/users")
+			.then((response) => response.json())
+			.then((data) => processUsers(data));
+
+		return <Leaderboard users={users} />;
+	} catch (error) {
+		// console.error(error);
+		console.log("Failed to load rankings");
+		return <div>Failed to load rankings</div>;
+	}
 }

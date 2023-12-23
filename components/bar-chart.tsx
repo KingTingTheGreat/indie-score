b9a00f0f235c96705195505267e557763c98689a
KingTingTheGreat <jting@bu.edu>
2023-12-23 14:44:06 -0500

    fixed a bug where multiple liness would be colored

--- components/bar-chart.tsx
+++ components/bar-chart.tsx
@@ -3,14 +3,15 @@ import { Chart } from "react-google-charts";
 
 const maxScore = 100;
 const title = ["Score", "Number of Users", { role: "style" }];
-let data: (number | string)[][] = [];
-for (let i = 0; i <= maxScore; i++) {
-	const num = Math.floor(Math.random() * 100) + 1;
-	data.push([i, num, "#78CB5F"]);
-}
 
 export const BarChart = ({ score }: { score: number }) => {
-	data[score][2] = "#FF0000";
+	let data: (number | string)[][] = [];
+	for (let i = 0; i <= maxScore; i++) {
+		const num = Math.floor(Math.random() * 100) + 1;
+		if (i === score) data.push([i, num, "#FF0000"]);
+		else data.push([i, num, "#78CB5F"]);
+	}
+	// data[score][2] = "#FF0000";
 
 	console.log("bar chart");
 	return (
@@ -23,7 +24,6 @@ export const BarChart = ({ score }: { score: number }) => {
 				// title: "Distribution of User Scores",
 				bar: { groupWidth: "100%" },
 				legend: { position: "none" },
-				colors: ["#FF0000", "#78CB5F"],
 				backgroundColor: "#444",
 				vAxis: {
 					title: "Number of Users",

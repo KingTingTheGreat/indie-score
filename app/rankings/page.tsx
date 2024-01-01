f217c00f4032894111a5ba26c7a4de08350c2796
KingTingTheGreat <jting@bu.edu>
2024-01-01 00:21:24 -0500

    deleted unused code

--- app/rankings/page.tsx
+++ app/rankings/page.tsx
@@ -13,13 +13,6 @@ const processUsers = (users: any) => {
 
 export default async function Rankings() {
 	try {
-		// const recipesResponse = await axios.get("http://localhost:3000/api/users"); // replace with deployment endpoint
-		// const recipes = recipesResponse.data;
-		// const users = processUsers(recipes);
-		// if (!(recipes instanceof Array)) {
-		// 	throw new Error("Recipes response data is not an array");
-		// }
-
 		const users = await fetch("https://indie-score.vercel.app/api/users")
 			.then((response) => response.json())
 			.then((data) => processUsers(data));

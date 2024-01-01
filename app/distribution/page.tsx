38d98576b95e80fbf452da50aa8303515329f62c
KingTingTheGreat <jting@bu.edu>
2024-01-01 00:22:09 -0500

    try-catch to prevent errors

--- app/distribution/page.tsx
+++ app/distribution/page.tsx
@@ -10,18 +10,24 @@ const processUsers = (users: any) => {
 };
 
 export default async function Distribution() {
-	const score = Math.floor(Math.random() * 100);
-	const users = await fetch("https://indie-score.vercel.app/api/users")
-		.then((response) => response.json())
-		.then((data) => processUsers(data));
-	return (
-		<main>
-			<div className="w-full flex justify-center">
-				<div className="flex flex-col justify-center items-center min-w-[90%] min-h-[60%] bg-[#555] m-4 ">
-					<p className="text-xl">Your Score: {score}</p>
-					<BarChart score={score} users={users} />
+	try {
+		const score = Math.floor(Math.random() * 100);
+		const users = await fetch("https://indie-score.vercel.app/api/users")
+			.then((response) => response.json())
+			.then((data) => processUsers(data));
+		return (
+			<main>
+				<div className="w-full flex justify-center">
+					<div className="flex flex-col justify-center items-center min-w-[90%] min-h-[60%] bg-[#555] m-4 ">
+						<p className="text-xl">Your Score: {score}</p>
+						<BarChart score={score} users={users} />
+					</div>
 				</div>
-			</div>
-		</main>
-	);
+			</main>
+		);
+	} catch (error) {
+		// console.error(error);
+		console.log("Failed to load user data");
+		return <div>Failed to load user data</div>;
+	}
 }

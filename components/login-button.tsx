2bb497e209a1df3f36819962ffb9607da854d436
KingTingTheGreat <jting@bu.edu>
2024-01-01 01:52:00 -0500

    created a login button

---
+++ components/login-button.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { signIn } from "next-auth/react";
+
+export const LoginButton = () => {
+	const handleLogin = () => {
+		signIn("spotify", { callbackUrl: "http://localhost:3000" });
+	};
+	return (
+		<button className="m-5 px-5 py-2 w-fit text-2xl rounded-2xl bg-[#78CB5F]" onClick={handleLogin}>
+			Login with Spotify
+		</button>
+	);
+};

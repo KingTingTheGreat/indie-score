8d1ed22109947c6927d0b158fee42433a4e08b4e
KingTingTheGreat <jting@bu.edu>
2024-01-01 02:49:44 -0500

    login button uses next-auth to login to spotify

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -3,8 +3,10 @@ import { signIn } from "next-auth/react";
 
 export const LoginButton = () => {
 	const handleLogin = () => {
+		console.log("login button clicked");
 		signIn("spotify", { callbackUrl: "http://localhost:3000" });
 	};
+
 	return (
 		<button className="m-5 px-5 py-2 w-fit text-2xl rounded-2xl bg-[#78CB5F]" onClick={handleLogin}>
 			Login with Spotify

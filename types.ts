ff9fe3756b19f05246f034d6eb3a2907d20083dc
KingTingTheGreat <jting@bu.edu>
2024-01-04 09:56:35 -0500

    types regarding returns from spotify

--- types.ts
+++ types.ts
@@ -20,4 +20,32 @@ export type DataArray = [number, number, string];
 export interface Session extends Omit<DefaultSession, "user"> {
 	user?: User;
 	expires: string;
+	score: number;
 }
+
+export type Album = {
+	images: {
+		url: string;
+	}[];
+	id: string;
+	name: string;
+};
+
+export type Artist = {
+	name: string;
+	id: string;
+};
+
+export type Song = {
+	album: Album;
+	artists: Artist[];
+	external_urls: { spotify: string };
+	href: string;
+	name: string;
+	popularity: number;
+	id: string;
+};
+
+export type SpotifyResponse = {
+	items: Song[];
+};

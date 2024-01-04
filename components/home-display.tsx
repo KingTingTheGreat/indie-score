57de6346820bd1a2efae9bff20f22b039e0fe7fd
KingTingTheGreat <jting@bu.edu>
2024-01-04 10:44:31 -0500

    corrected types. still need to fix session user types

--- components/home-display.tsx
+++ components/home-display.tsx
@@ -3,21 +3,24 @@ import { useEffect, useState } from "react";
 import { SongIcon } from "./song-icon";
 import { LoginButton } from "./login-button";
 import { useSession } from "next-auth/react";
+import { Song } from "@/types";
 
 export const HomeDisplay = () => {
-	const [data, setData] = useState(undefined);
+	const [data, setData] = useState<Song[]>([]);
 	const { data: session } = useSession();
 
 	useEffect(() => {
 		const fetchSongs = async () => {
+			// @ts-ignore
 			if (session?.user?.accessToken) {
 				const response = await fetch("/api/tracks", {
 					headers: {
+						// @ts-ignore
 						AccessToken: session.user.accessToken,
 					},
 				});
 				const resData = await response.json();
-				setData(resData);
+				setData(resData.items);
 			} else {
 				console.log("no session or user or accessToken");
 				console.log(session);
@@ -29,9 +32,11 @@ export const HomeDisplay = () => {
 	return (
 		<main className="flex min-h-screen w-[80%] bg-[#987500] flex-wrap items-center justify-between p-24">
 			<LoginButton />
-			{data && data.items
-				? data.items.map((song) => <SongIcon key={song.id} song={song} />)
-				: console.log(`no items to map ${data}`)}
+			{data ? (
+				data.map((song) => <SongIcon key={song.id} song={song} />)
+			) : (
+				<p>no data to display. please sign in</p>
+			)}
 		</main>
 	);
 };

9cdbaf18e3f29034e75fbdbdf1588c720ea19db9
KingTingTheGreat <jting@bu.edu>
2024-01-05 11:44:53 -0500

    trying to restrict access to this api endpoint

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,17 +1,27 @@
 import { type NextRequest } from "next/server";
 import { usersDBConnect } from "@/utils/connection";
-import { ResponseFuncs } from "@/types";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
 export async function GET(request: NextRequest) {
+	if (request.nextUrl.origin !== process.env.ALLOWED_ORIGIN) {
+		return new Response("Unauthorized", {
+			status: 401,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET",
+				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			},
+		});
+	}
+
 	const db = await usersDBConnect();
 	const users = await db.Users.find({});
 
 	return new Response(JSON.stringify(users), {
 		status: 200,
 		headers: {
-			"Access-Control-Allow-Origin": "/",
+			"Access-Control-Allow-Origin": "*",
 			"Access-Control-Allow-Methods": "GET",
 			"Access-Control-Allow-Headers": "Content-Type, Authorization",
 		},

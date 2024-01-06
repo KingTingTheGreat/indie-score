e75977bf0f26fb2de9db55bc392234c1b249350f
KingTingTheGreat <jting@bu.edu>
2024-01-06 10:11:40 -0500

    removed auth for this endpoint. this is simply a call to spotify api and does not provide any relevant access to a bad actor

--- app/api/tracks/route.ts
+++ app/api/tracks/route.ts
@@ -9,32 +9,32 @@ const url = `https://api.spotify.com/v1/me/top/tracks?limit=10&time_range=short_
 export async function GET(request: NextRequest) {
 	try {
 		const headersList = headers();
-		if (
-			crypto.timingSafeEqual(
-				Buffer.from(headersList.get("authorization") as string),
-				Buffer.from(process.env.API_AUTH as string)
-			)
-		) {
-			const token = headersList.get("AccessToken");
-			const response = await fetch(url, {
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			});
+		// if (
+		// 	crypto.timingSafeEqual(
+		// 		Buffer.from(headersList.get("authorization") as string),
+		// 		Buffer.from(process.env.API_AUTH as string)
+		// 	)
+		// ) {
+		const token = headersList.get("AccessToken");
+		const response = await fetch(url, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
 
-			const data = await response.json();
+		const data = await response.json();
 
-			return new Response(JSON.stringify(data), {
-				status: 200,
-				headers: {
-					"Access-Control-Allow-Origin": "/",
-					"Access-Control-Allow-Methods": "GET, POST",
-					"Access-Control-Allow-Headers": "Content-Type, Authorization",
-				},
-			});
-		} else {
-			throw new Error("Unauthorized");
-		}
+		return new Response(JSON.stringify(data), {
+			status: 200,
+			headers: {
+				"Access-Control-Allow-Origin": "/",
+				"Access-Control-Allow-Methods": "GET, POST",
+				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			},
+		});
+		// } else {
+		// 	throw new Error("Unauthorized");
+		// }
 	} catch (error) {
 		return new Response(JSON.stringify("Unauthorized"), {
 			status: 401,

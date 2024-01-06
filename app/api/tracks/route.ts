ff51d2c4808afdbfc75f99e0992819136496122c
KingTingTheGreat <jting@bu.edu>
2024-01-06 09:28:28 -0500

    added authentication to api endpoints

--- app/api/tracks/route.ts
+++ app/api/tracks/route.ts
@@ -1,26 +1,48 @@
 import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+import crypto from "crypto";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
 const url = `https://api.spotify.com/v1/me/top/tracks?limit=10&time_range=short_term`;
 
 export async function GET(request: NextRequest) {
-	const token = request.headers.get("AccessToken");
+	try {
+		const headersList = headers();
+		if (
+			crypto.timingSafeEqual(
+				Buffer.from(headersList.get("authorization") as string),
+				Buffer.from(process.env.API_AUTH as string)
+			)
+		) {
+			const token = headersList.get("AccessToken");
+			const response = await fetch(url, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
 
-	const response = await fetch(url, {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	});
+			const data = await response.json();
 
-	const data = await response.json();
-
-	return new Response(JSON.stringify(data), {
-		status: 200,
-		headers: {
-			"Access-Control-Allow-Origin": "/",
-			"Access-Control-Allow-Methods": "GET, POST",
-			"Access-Control-Allow-Headers": "Content-Type, Authorization",
-		},
-	});
+			return new Response(JSON.stringify(data), {
+				status: 200,
+				headers: {
+					"Access-Control-Allow-Origin": "/",
+					"Access-Control-Allow-Methods": "GET, POST",
+					"Access-Control-Allow-Headers": "Content-Type, Authorization",
+				},
+			});
+		} else {
+			throw new Error("Unauthorized");
+		}
+	} catch (error) {
+		return new Response(JSON.stringify("Unauthorized"), {
+			status: 401,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET",
+				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			},
+		});
+	}
 }
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,15 +1,33 @@
 import { type NextRequest } from "next/server";
 import { usersDBConnect } from "@/utils/connection";
+import { headers } from "next/headers";
+import crypto from "crypto";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
 export async function GET(request: NextRequest) {
-	console.log(request);
-	console.log(request.nextUrl);
-	console.log(request.nextUrl.origin);
-
-	if (request.nextUrl.origin !== process.env.ALLOWED_ORIGIN) {
-		return new Response("Unauthorized", {
+	try {
+		if (
+			crypto.timingSafeEqual(
+				Buffer.from(headers().get("authorization") as string),
+				Buffer.from(process.env.API_AUTH as string)
+			)
+		) {
+			const db = await usersDBConnect();
+			const users = await db.Users.find({});
+			return new Response(JSON.stringify(users), {
+				status: 200,
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+					"Access-Control-Allow-Methods": "GET",
+					"Access-Control-Allow-Headers": "Content-Type, Authorization",
+				},
+			});
+		} else {
+			throw new Error("Unauthorized");
+		}
+	} catch (error) {
+		return new Response(JSON.stringify("Unauthorized"), {
 			status: 401,
 			headers: {
 				"Access-Control-Allow-Origin": "*",
@@ -18,16 +36,4 @@ export async function GET(request: NextRequest) {
 			},
 		});
 	}
-
-	const db = await usersDBConnect();
-	const users = await db.Users.find({});
-
-	return new Response(JSON.stringify(users), {
-		status: 200,
-		headers: {
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Methods": "GET",
-			"Access-Control-Allow-Headers": "Content-Type, Authorization",
-		},
-	});
 }

3e2c3e1c3d15536a3a7ab539f264dbe2cf114d8b
KingTingTheGreat <jting@bu.edu>
2024-01-06 23:53:19 -0700

    testing

--- app/api/tracks/route.ts
+++ app/api/tracks/route.ts
@@ -4,40 +4,21 @@ import crypto from "crypto";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
-const url = `https://api.spotify.com/v1/me/top/tracks?limit=10&time_range=short_term`;
-
 export async function GET(request: NextRequest) {
 	try {
 		const headersList = headers();
-		// if (
-		// 	crypto.timingSafeEqual(
-		// 		Buffer.from(headersList.get("authorization") as string),
-		// 		Buffer.from(process.env.API_AUTH as string)
-		// 	)
-		// ) {
 		const token = headersList.get("AccessToken");
-		const response = await fetch(url, {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		});
-
-		const data = await response.json();
-
-		return new Response(JSON.stringify(data), {
+		return new Response(JSON.stringify({ "your token": token }), {
 			status: 200,
 			headers: {
-				"Access-Control-Allow-Origin": "/",
+				"Access-Control-Allow-Origin": "*",
 				"Access-Control-Allow-Methods": "GET, POST",
 				"Access-Control-Allow-Headers": "Content-Type, Authorization",
 			},
 		});
-		// } else {
-		// 	throw new Error("Unauthorized");
-		// }
 	} catch (error) {
-		return new Response(JSON.stringify("Unauthorized"), {
-			status: 401,
+		return new Response(JSON.stringify("Something Went Wrong"), {
+			status: 500,
 			headers: {
 				"Access-Control-Allow-Origin": "*",
 				"Access-Control-Allow-Methods": "GET",
@@ -46,3 +27,46 @@ export async function GET(request: NextRequest) {
 		});
 	}
 }
+
+// const url = `https://api.spotify.com/v1/me/top/tracks?limit=10&time_range=short_term`;
+
+// export async function GET(request: NextRequest) {
+// 	try {
+// 		const headersList = headers();
+// 		// if (
+// 		// 	crypto.timingSafeEqual(
+// 		// 		Buffer.from(headersList.get("authorization") as string),
+// 		// 		Buffer.from(process.env.API_AUTH as string)
+// 		// 	)
+// 		// ) {
+// 		const token = headersList.get("AccessToken");
+// 		const response = await fetch(url, {
+// 			headers: {
+// 				Authorization: `Bearer ${token}`,
+// 			},
+// 		});
+
+// 		const data = await response.json();
+
+// 		return new Response(JSON.stringify(data), {
+// 			status: 200,
+// 			headers: {
+// 				"Access-Control-Allow-Origin": "/",
+// 				"Access-Control-Allow-Methods": "GET, POST",
+// 				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+// 			},
+// 		});
+// 		// } else {
+// 		// 	throw new Error("Unauthorized");
+// 		// }
+// 	} catch (error) {
+// 		return new Response(JSON.stringify("Unauthorized"), {
+// 			status: 401,
+// 			headers: {
+// 				"Access-Control-Allow-Origin": "*",
+// 				"Access-Control-Allow-Methods": "GET",
+// 				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+// 			},
+// 		});
+// 	}
+// }

8f2a7c63d68fa4b437132b9bc04cff682278327e
KingTingTheGreat <jting@bu.edu>
2024-01-07 22:32:57 -0700

    update name

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const Logo = () => {
 	return (
 		<Link href="/" className="font-superindie">
-			<h2 className="text-6xl">Indie Ranker</h2>
+			<h2 className="text-6xl">Indiefy</h2>
 		</Link>
 	);
 };

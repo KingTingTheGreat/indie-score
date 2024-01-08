6a49b2e2a6f6ee1b4705356ab87b09dcdd7c4879
KingTingTheGreat <jting@bu.edu>
2024-01-07 20:13:44 -0700

    removing auth for testing

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -7,25 +7,25 @@ export const dynamic = "force-dynamic"; // defaults to auto
 
 export async function GET(request: NextRequest) {
 	try {
-		if (
-			crypto.timingSafeEqual(
-				Buffer.from(headers().get("authorization") as string),
-				Buffer.from(process.env.API_AUTH as string)
-			)
-		) {
-			const db = await usersDBConnect();
-			const users = await db.Users.find({});
-			return new Response(JSON.stringify(users), {
-				status: 200,
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Methods": "GET",
-					"Access-Control-Allow-Headers": "Content-Type, Authorization",
-				},
-			});
-		} else {
-			throw new Error("Unauthorized");
-		}
+		// if (
+		// 	crypto.timingSafeEqual(
+		// 		Buffer.from(headers().get("authorization") as string),
+		// 		Buffer.from(process.env.API_AUTH as string)
+		// 	)
+		// ) {
+		const db = await usersDBConnect();
+		const users = await db.Users.find({});
+		return new Response(JSON.stringify(users), {
+			status: 200,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET",
+				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			},
+		});
+		// } else {
+		// 	throw new Error("Unauthorized");
+		// }
 	} catch (error) {
 		return new Response(JSON.stringify("Unauthorized"), {
 			status: 401,

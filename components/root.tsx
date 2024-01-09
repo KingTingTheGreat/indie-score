6dad6d6cca6f1c85734c6ba0677f5e90c00c36eb
KingTingTheGreat <jting@bu.edu>
2024-01-08 22:07:39 -0700

    added footer

--- components/root.tsx
+++ components/root.tsx
@@ -1,21 +1,19 @@
 "use client";
 import { Inter } from "next/font/google";
 import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const Root = ({ children }: { children: React.ReactNode }) => {
 	return (
-		// <SessionProvider session={pageProps.session}>
-		// 	<Header />
-		// 	{children}
-		// </SessionProvider>
 		<SessionProvider>
 			<html lang="en">
 				<body className={inter.className}>
 					<Header />
 					{children}
+					<Footer />
 				</body>
 			</html>
 		</SessionProvider>
